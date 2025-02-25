111e7e54af003674ab9e4d4cc1908cafff0cf0ee
Павел Солнцев <pavelsolncev@MacBook-Air-Pavel.local>
2025-02-25 09:45:51 +0700

    fix

--- buttons/buttonGoal.js
+++ buttons/buttonGoal.js
@@ -4,29 +4,30 @@ module.exports = (bot, GlobalState) => {
   bot.action(/goal_(\d+)_(\d+)/, async (ctx) => {
     const teamIndex = parseInt(ctx.match[1], 10);
     const playerIndex = parseInt(ctx.match[2], 10);
-    const playingTeams = GlobalState.getPlayingTeams();
+    const teams = GlobalState.getTeams();
   
-    if (!playingTeams) {
-      return ctx.answerCbQuery("⛔ Нет активного матча!");
-    }
-  
-    let player;
-    if (teamIndex === playingTeams.teamIndex1) {
-      player = playingTeams.team1[playerIndex];
-      player.goals = (player.goals || 0) + 1;
-    } else if (teamIndex === playingTeams.teamIndex2) {
-      player = playingTeams.team2[playerIndex];
-      player.goals = (player.goals || 0) + 1;
-    } else {
+    if (!teams[teamIndex] || !teams[teamIndex][playerIndex]) {
       return ctx.answerCbQuery("Игрок не найден!");
     }
   
-    GlobalState.setPlayingTeams(playingTeams);
+    const player = teams[teamIndex][playerIndex];
+    player.goals = (player.goals || 0) + 1;
+    GlobalState.setTeams(teams);
   
-    await updatePlayingTeamsMessage(ctx);
+    // Обновляем playingTeams
+    const playingTeams = GlobalState.getPlayingTeams();
+    if (playingTeams) {
+      if (teamIndex === playingTeams.teamIndex1) {
+        playingTeams.team1[playerIndex].goals = player.goals;
+      } else if (teamIndex === playingTeams.teamIndex2) {
+        playingTeams.team2[playerIndex].goals = player.goals;
+      }
+      GlobalState.setPlayingTeams(playingTeams);
+    }
   
+    await updatePlayingTeamsMessage(ctx); // Теперь сообщение обновится
+
     const message = await ctx.reply(`Гол добавлен для ${player.name}! Теперь у него ${player.goals} гол(ов).`);
     return deleteMessageAfterDelay(ctx, message.message_id);
   });
-  
 };
